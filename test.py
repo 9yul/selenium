bf266edfd6408373b324cb478958cc211bb12a50
9yul <schu80927@gmail.com>
2021-01-03 19:33:06 +0900

    셀레니움 예제

---
+++ test.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import urllib.request
+
+driver = webdriver.Chrome()
+driver.get("https://www.google.co.kr/imghp?hl=ko&tab=ri&ogbl")
+elem = driver.find_element_by_name("q")
+elem.send_keys("파인애플.png")
+elem.send_keys(Keys.RETURN)
+
+SCROLL_PAUSE_TIME = 0.5
+
+# Get scroll height
+last_height = driver.execute_script("return document.body.scrollHeight")
+
+while True:
+    # Scroll down to bottom
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    # Wait to load page
+    time.sleep(SCROLL_PAUSE_TIME)
+
+    # Calculate new scroll height and compare with last scroll height
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        break
+    last_height = new_height
+
+
+images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
+count = 99
+for image in images:
+    try:
+        image.click()
+        time.sleep(3)
+        imgUrl = driver.find_element_by_css_selector(".n3VNCb").get_attribute("src")
+        urllib.request.urlretrieve(imgUrl, str(count) + ".jpg")
+        count = count + 1
+    except:
+        pass
+# assert "Python" in driver.title
+# elem = driver.find_element_by_name("q")
+# elem.clear()
+# elem.send_keys("pycon")
+# elem.send_keys(Keys.RETURN)
+# assert "No results found." not in driver.page_source
+# driver.close()
